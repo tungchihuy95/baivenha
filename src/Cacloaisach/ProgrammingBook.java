c14450b4f691881b6c737f328bcd8e9423e29ece
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-14 11:58:20 +0700

    jjhkag

---
+++ src/Cacloaisach/ProgrammingBook.java
@@ -0,0 +1,32 @@
+package Cacloaisach;
+
+import Cacloaisach.Book;
+
+public class ProgrammingBook extends Book {
+    String language;
+    String framework;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+
+    public ProgrammingBook(String bookCode, String name, int price, String author, String language, String framework) {
+        super(bookCode, name, price, author);
+        this.language = language;
+        this.framework = framework;
+
+
+    }
+}
